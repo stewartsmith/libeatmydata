b02fe8ed7cbc8c64a37a91a64c8f8e0f6e9abd90
Eric Wong <normalperson@yhbt.net>
2009-01-26 00:52:56 -0800

    memoize dlsym lookup for open(2)
    This allows us to avoids the overhead of calling dlsym and error
    checking each time open(2) is called.  This is probably gcc-dependent,
    but I think RTLD_NEXT implies we're on a GNU system anyways.

--- eatmydata.c
+++ eatmydata.c
@@ -27,6 +27,15 @@
 
 int errno;
 
+static int (*libc_open)(const char*, int, ...);
+
+void __attribute__ ((constructor)) eatmydata_init(void)
+{
+        libc_open = dlsym(RTLD_NEXT, "open");
+        if (!libc_open || dlerror())
+                _exit(1);
+}
+
 int fsync(int fd)
 {
 	errno=0;
@@ -37,17 +46,10 @@ int open(const char* pathname, int flags, ...)
 {
 	va_list ap;
 	mode_t mode;
-	int (*libc_open)(const char*,int,...);
 
 	va_start(ap, flags);
 	mode= va_arg(ap, mode_t);
 	va_end(ap);
-
-	*(void**)(&libc_open) = dlsym(RTLD_NEXT, "open");
-	if(dlerror()) {
-		errno = EACCES;
-		return -1;
-	}
 
 	flags &= ~(O_SYNC|O_DSYNC);
 
