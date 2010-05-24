2146529816fc1713f5c6b5344bbeb760257290fb
Monty Taylor <mordred@inaugust.com>
2010-05-24 08:14:19 -0700

    License header.

--- libeatmydata/test/fsynctest.c
+++ libeatmydata/test/fsynctest.c
@@ -1,18 +1,20 @@
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+/* BEGIN LICENSE
+ * Copyright (C) 2008-2010 Stewart Smith <stewart@flamingspork.com>
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License version 3, as published 
+ * by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranties of 
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+ * PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * END LICENSE */
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+#include "config.h"
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- */
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
